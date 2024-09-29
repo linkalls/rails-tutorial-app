f318fdbd269fd7b09e393b94338401abfef1cc35
linkalls <naonyan1135@gmail.com>
2024-09-30 00:33:40 +0900

    root_urlが使えるようになる

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get root_url" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -23,10 +29,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
- test "show get contact " do
-  get static_pages_contact_url
-  assert_response :success
-  assert_select "title", "Contact | #{@base_title}"
- end
-
+  test "show get contact " do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
 end
