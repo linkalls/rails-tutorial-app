7394b4e9f85a3913fc4c5393457cdd68681201c2
linkalls <naonyan1135@gmail.com>
2024-10-03 00:30:22 +0900

    provideを消せばいい

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def full_title(page_title: "")
+  def full_title(page_title = "") # イコールだよ
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty? # if page_title.empty?で文字列が空かどうか
       base_title
