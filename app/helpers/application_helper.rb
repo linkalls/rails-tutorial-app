04b2080236d4ad8707d89e510b4d2632837eda40
linkalls <naonyan1135@gmail.com>
2024-10-03 00:22:42 +0900

    .empty?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title: "")
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty? # if page_title.empty?で文字列が空かどうか
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
